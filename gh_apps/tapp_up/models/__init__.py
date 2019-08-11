c23077070e158b842620a5647be7ac7b156618e5
phuongnguyentqn <phuongnguyenab6@gmail.com>
2019-08-11 15:18:10 +0700

    Add model Expense

--- gh_apps/tapp_up/models/__init__.py
+++ gh_apps/tapp_up/models/__init__.py
@@ -1,1 +1,2 @@
 from .user import Grasshopper
+from .expense import Expense
--- gh_apps/tapp_up/models/expense.py
+++ gh_apps/tapp_up/models/expense.py
@@ -1,15 +1,23 @@
-# """
-# Model User
-# """
-# from django.db import models
+"""
+Model User
+"""
+from django.conf import settings
+from django.db import models
 
-# from tapp_up.models.base import BaseModel
+from tapp_up.models.base import BaseModel
 
 
-# class Expense(BaseModel):
-#     """
-#     Define all attributes and methods of Expense
-#     """
-#     consumer = models.ForeignKey('User', )
-#     def __str__(self):
-#         pass
+class Expense(BaseModel):
+    """
+    Define all attributes and methods of Expense
+    """
+    consumer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
+    )
+    quantity = models.IntegerField()
+    expense_date = models.DateTimeField()
+    good_type = models.CharField(max_length=126)
+
+    def __str__(self):
+        return f'{self.quantity} VND by User[{self.consumer}]' \
+            ' on {self.good_type} at {self.expense_date}'
